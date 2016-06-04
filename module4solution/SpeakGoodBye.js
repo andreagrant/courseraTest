f1e1a5d776419160a56fa64d61fdb0ff64002a61
Andrea Grant <gran0260@umn.edu>
2016-06-04 08:09:15 -0700

    tested; removed wrong args to speaker

--- module4solution/SpeakGoodBye.js
+++ module4solution/SpeakGoodBye.js
@@ -5,7 +5,6 @@
   // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
   // See Lecture 52, part 2
 
-
   // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
   // the "speak" method and which you will expose to the global context
   // See Lecture 52, part 1
@@ -24,4 +23,4 @@
   // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
   // 'byeSpeaker' on the global scope as well.
   window.byeSpeaker = byeSpeaker;
-})(window);
+})(window,name);
